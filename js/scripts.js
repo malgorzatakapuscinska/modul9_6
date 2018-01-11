a56e2c6c211870a499679631c91aecd962b072f3
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-11 19:12:35 +0100

    Ready task

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var list = document.getElementById('list');  //wybieramy element ul
-var add = document.getElementById('addElem');//wybieramy przycisk
+var list = document.getElementById('list');  //wybieramy element ul 
+var add = document.getElementById('addElem');//wybieramy przycisk 
 
-add.addEventListener('click',function () {
-	var liElements = document.getElementsByTagName('li');
-	var element = document.createElement('li');
-	element.innerHTML = "Item " + liElements.length;
-	list.appendChild(element);
-});
+add.addEventListener('click',function () { 
+var liElements = document.getElementsByTagName('li'); 
+var element = document.createElement('li'); 
+element.innerHTML= "Item " + liElements.length; 
+list.appendChild(element); 
+}); 
