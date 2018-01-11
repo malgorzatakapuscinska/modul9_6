c51ac03f58fd479a3211086b3312f159178c254d
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-11 14:02:07 +0100

    Unexpected bug - to be fixed

---
+++ js/testing.js
@@ -0,0 +1,11 @@
+function checkOnclickEvent(param) {
+  console.log(param);
+}
+var inputElem = document.getElementById('js-inputValue'),
+    imageElem = document.getElementById('js-image');
+
+imageElem.addEventListener('click', function(e) {
+  checkOnclickEvent('image was clicked')
+});
+
+inputElem.addEventListene
