962838c722abadcf92657d161e41c83722ca4c64
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-12 09:09:20 +0100

    Add list instead of document in var liElements = list.getElementsByTagName('li');

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');  //wybieramy element ul
 var add = document.getElementById('addElem');//wybieramy przycisk 
 
 add.addEventListener('click',function () { 
-var liElements = document.getElementsByTagName('li'); 
+var liElements = list.getElementsByTagName('li'); 
 var element = document.createElement('li'); 
 element.innerHTML= "Item " + liElements.length; 
 list.appendChild(element); 
